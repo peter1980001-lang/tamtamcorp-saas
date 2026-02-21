ebf2dc5199bc907ac6ca96d464557bdc71dc5136
Petermeter123 <peter1980.001@gmail.com>
2026-02-21 14:03:08 +0400

    Update layout.tsx

--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -39,7 +39,7 @@ export default async function AdminLayout({ children }: { children: React.ReactN
   return (
     <>
       {/* Client-side idle logout (nur /admin) */}
-      <IdleLogoutGate redirectTo="/login" />
+      <IdleLogoutGate warningMs={15_000} logoutMs={20_000} redirectTo="/login" />
       {children}
     </>
   );
